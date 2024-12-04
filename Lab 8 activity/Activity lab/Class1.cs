6d270d3c475aa2f03cc47e266b4cffdd44eb7bf0
wasay-sial <233511@students.au.edu.pk>
2024-12-04 07:13:09 +0500

    Add files via upload

---
+++ Lab 8 activity/Activity lab/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_lab
+{
+    internal class Validation
+    {
+        public void chkName(string n)
+        {
+            if (n.Length > 15)
+            {
+                throw new Exception("Name must be less than 15 characters");
+
+            }
+            else if (n.Length == 0)
+            {
+                throw new Exception("Name is required");
+            }
+        }
+    }
+}
---
+++ Lab 8 activity/Activity lab/form 2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_lab
+{
+    public partial class form_2 : Form
+    {
+        public form_2()
+        {
+            InitializeComponent();
+        }
+        public form_2(string name,string gender,string hobbies,string status,string country)
+        {
+            InitializeComponent();
+
+            label1.Text = "Name is " + name + " \n" +
+                        "Gender is " + gender + " \n" +
+                        "Hobby is " + hobbies + " \n" +
+                        "Status is " + status + " \n" +
+                        "Country is " + country + " \n";
+
+            label1.Show();
+
+        }
+
+        private void form_2_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
