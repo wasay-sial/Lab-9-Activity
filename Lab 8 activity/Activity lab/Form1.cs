eab8b1b6fe2dde0d0066e856f3940bfca45d7015
wasay-sial <233511@students.au.edu.pk>
2024-12-04 13:12:27 +0500

    Add files via upload

---
+++ Lab 8 activity/Activity lab/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_lab
+{
+    public partial class Form1 : Form
+    {
+
+        string gender;
+        string hobbies;
+        string status;
+        string name;
+        string country;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+           string[] countries = { "Pakistan", "bangladesh", "USA", "Canada", "Ireland" };
+           foreach (string country in countries)
+           {
+                comboBox1.Items.Add(country);
+           }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            if(radioButton1.Checked)
+            {
+                gender = radioButton1.Text;
+            }
+            else
+            {
+                gender = radioButton2.Text;
+            }
+
+            if(radioButton3.Checked)
+            {
+                status = radioButton3.Text;
+            }
+            else
+            {
+                status = radioButton4.Text;
+            }
+
+            if(checkBox1.Checked)
+            {
+                hobbies = checkBox1.Text;
+
+            }
+            else
+            {
+                hobbies = checkBox2.Text;
+            }
+
+            country = comboBox1.SelectedItem.ToString();
+
+            name = textBox1.Text;
+
+            Validation v1=new Validation();
+            v1.chkName(name);
+            
+
+            form_2 obj = new form_2(name,gender,hobbies,status,country);
+            obj.ShowDialog();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+        private void load_customer()
+        {
+            string sqlconn = "Data Source=AUMC-LAB3-25\\SQLEXPRESS;Initial Catalog=Customer_VS;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(sqlconn);
+            conn.Open();
+
+            string query = "select * from Customer_info";
+            //string query = "select Country from C_info where Status=0";
+
+            SqlCommand command = new SqlCommand(query, conn);
+
+            DataSet d1 = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(d1);
+            dataGridView1.DataSource = d1.Tables[0];
+
+            conn.Close();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            load_customer();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                gender = radioButton1.Text;
+            }
+            else
+            {
+                gender = radioButton2.Text;
+            }
+
+            if (radioButton3.Checked)
+            {
+                status = "1";
+            }
+            else
+            {
+                status = "0";
+            }
+
+            if (checkBox1.Checked)
+            {
+                hobbies = checkBox1.Text;
+
+            }
+            else
+            {
+                hobbies = checkBox2.Text;
+            }
+
+            country = comboBox1.SelectedItem.ToString();
+
+            name = textBox1.Text;
+
+            string sqlconn = "Data Source=AUMC-LAB3-25\\SQLEXPRESS;Initial Catalog=Customer_VS;Integrated Security=True";
+
+            SqlConnection conn=new SqlConnection( sqlconn );
+            conn.Open();
+
+            string command= "Insert into Customer_info values ('" + name+"','"+country+"','"+gender+"','"+hobbies+"',"+status+")";
+
+            SqlCommand insertion=new SqlCommand(command,conn);
+            insertion.ExecuteNonQuery();
+            conn.Close();
+
+            load_customer();
+
+            //dataGridView
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            string sqlconn = "Data Source=AUMC-LAB3-25\\SQLEXPRESS;Initial Catalog=Customer_VS;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(sqlconn);
+            conn.Open();
+
+            string command = "delete from Customer_info where CustomerName='"+name+"'";
+
+            SqlCommand deletion = new SqlCommand(command, conn);
+            deletion.ExecuteNonQuery();
+            conn.Close();
+
+            load_customer();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            clearForm();
+            string CustomerName = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            display_cust(CustomerName);
+        }
+
+        private void display_cust(string s)
+        {
+            string sqlconn = "Data Source=AUMC-LAB3-25\\SQLEXPRESS;Initial Catalog=Customer_VS;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(sqlconn);
+            conn.Open();
+
+            string query = "select * from Customer_info";
+            //string query = "select Country from C_info where Status=0";
+
+            SqlCommand command = new SqlCommand(query, conn);
+
+            DataSet d1 = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(d1);
+            conn.Close();
+
+            textBox1.Text = d1.Tables[0].Rows[0][0].ToString();
+            comboBox1.Text = d1.Tables[0].Rows[0][1].ToString();
+
+            string Gender = d1.Tables[0].Rows[0][2].ToString();
+            if (Gender.Equals("Male"))
+            {
+                radioButton1.Checked = true;
+            }
+            else
+            {
+                radioButton2.Checked = true;
+            }
+
+            string Hobby = d1.Tables[0].Rows[0][3].ToString();
+            if (Hobby.Equals("Reading"))
+            {
+                checkBox1.Checked = true;
+            }
+            else
+            {
+                checkBox2.Checked = true;
+            }
+            string Married = d1.Tables[0].Rows[0][4].ToString();
+            if (Married.Equals("True"))
+            {
+                radioButton3.Checked = true;
+            }
+            else
+            {
+                radioButton4.Checked = true;
+            }
+        }
+
+        private void clearForm()
+        {
+            name = "";
+            country = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked= false;
+            radioButton4.Checked = false;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab 8 activity/Activity lab/Form3.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Activity_lab
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.customerInfoPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.customerInfoPreviewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 38);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // customerInfoPreviewToolStripMenuItem
+            // 
+            this.customerInfoPreviewToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerInfoPreviewToolStripMenuItem.Name = "customerInfoPreviewToolStripMenuItem";
+            this.customerInfoPreviewToolStripMenuItem.Size = new System.Drawing.Size(248, 34);
+            this.customerInfoPreviewToolStripMenuItem.Text = "Customer info preview ";
+            this.customerInfoPreviewToolStripMenuItem.Click += new System.EventHandler(this.customerInfoPreviewToolStripMenuItem_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 222);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem customerInfoPreviewToolStripMenuItem;
+    }
+}
